49c45797998a09599f9d3b83d5dcbcf6040774a2
ivoferroo <1151159@isep.ipp.pt>
2017-01-08 15:46:14 +0000

    improves scripts [implementation]

---
+++ functions.sql
@@ -0,0 +1,27 @@
+-- Authors
+--
+-- Daniel Gonçalves - 1151452
+-- Eric Amaral - 1141570
+-- Ivo Ferro - 1151159
+-- Tiago Correia - 1151031
+
+-- get all projects
+CREATE OR REPLACE FUNCTION GET_PROJECTS
+RETURN SYS_REFCURSOR
+IS
+  C1 SYS_REFCURSOR;
+BEGIN
+  OPEN C1 FOR
+  SELECT * FROM PROJECT;
+  
+  RETURN C1;
+  
+EXCEPTION
+  WHEN NO_DATA_FOUND THEN
+    DBMS_OUTPUT.PUT_LINE('There was no data found'||SYSDATE);
+  WHEN TOO_MANY_ROWS THEN
+    DBMS_OUTPUT.PUT_LINE('Too many rows'||SYSDATE);
+  WHEN OTHERS THEN
+    DBMS_OUTPUT.PUT_LINE('An error occured'||SYSDATE);
+END;
+/
