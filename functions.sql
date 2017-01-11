cf0f5951f1fda0886f484fa7e05309b9dc5a4264
EricAmaral <1141570@isep.ipp.pt>
2017-01-11 00:03:39 +0000

    adds function getAirport

--- functions.sql
+++ functions.sql
@@ -54,3 +54,27 @@ BEGIN
      
 END FC_GET_ID_COORDINATE;
 
+-------------------------------------------------------------------
+
+-- function that get the airport 
+create or replace FUNCTION FC_GET_AIRPORT (L_IATA IN VARCHAR, L_ID_PROJECT IN INTEGER)
+RETURN SYS_REFCURSOR
+IS
+  C1 SYS_REFCURSOR;
+BEGIN
+  OPEN C1 FOR
+  select ar.IATA, ar.NAME, ar.TOWN, ar.COUNTRY, ar.ALTITUDE, cd.COORDINATE_CODE, cd.LATITUDE, cd.LONGITUDE
+  from AIRPORT ar, COORDINATE cd
+  where ar.ID_PROJECT = cd.ID_PROJECT
+  and ar.ID_COORDINATE = cd.ID_COORDINATE;
+  
+  RETURN C1;
+  
+EXCEPTION
+  WHEN NO_DATA_FOUND THEN
+    DBMS_OUTPUT.PUT_LINE('There was no data found'||SYSDATE);
+  WHEN TOO_MANY_ROWS THEN
+    DBMS_OUTPUT.PUT_LINE('Too many rows'||SYSDATE);
+  WHEN OTHERS THEN
+    DBMS_OUTPUT.PUT_LINE('An error occured'||SYSDATE);
+END;
